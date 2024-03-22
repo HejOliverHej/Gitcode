5becf9ea1793b250475c2c521c8e4920dea98665
Oliver Bäfverholm <oliver.bafverholm@learnet.se>
2024-03-22 09:23:00 +0100

    first commit: added code

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Gitcode
+{
+
+
+    internal class program
+    {
+
+        static void Main(string[] args)
+        {
+            int run = 0;
+            while (true)
+            {
+                Console.WriteLine("Välkommen");
+                Console.WriteLine("Välj i menyn (ett heltal");
+                run = int.Parse(Console.ReadLine());
+                if (run == 1) 
+                {
+                    Greetings();
+                }
+                else if (run == 2)
+                {
+
+                    Oliver();
+                }
+                else 
+                {
+                    Console.WriteLine("Hej då");
+                }
+
+            }
+
+            static void Greetings() 
+            {
+                Console.WriteLine("Hello from gihub");
+            }
+
+            static void Oliver() 
+            {
+                Console.WriteLine("Hello from Oliver");
+            }
+
+        }
+
+    }
+}
+    
+
+
+
+
+
+
+
+    
